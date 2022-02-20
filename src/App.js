75229d2abf723035444800ca5a717a7f7ff32a19
martin jaime <martinjosejaime@gmail.com>
2022-02-20 16:34:46 -0300

    Adding NewCustomerContainer and insertCustomer POST

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomeContainer from './containers/HomeContainer';
 import CustomersContainer from './containers/CustomersContainer';
 import CustomerContainer from './containers/CustomerContainer';
+import NewCustomerContainer from './containers/NewCustomerContainer';
 
 class App extends Component {
 
@@ -21,9 +22,10 @@ class App extends Component {
           <Route exact path="/" component={HomeContainer} />
           <Route exact path="/customers" component={CustomersContainer} />
           <Switch>
-            {/* <Route path="/customers/new" component={NewCustomerContainer} /> */}
+            <Route path="/customers/new" component={NewCustomerContainer} />
             <Route path="/customers/:dni" 
-                    render={props => <CustomerContainer dni={props.match.params.dni} />} />
+            render={props => <CustomerContainer dni={props.match.params.dni} />} 
+            />
           </Switch>
         </div>
       </Router>
---
+++ src/containers/NewCustomerContainer.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux' 
+import { withRouter } from 'react-router-dom'
+import { SubmissionError } from 'redux-form'
+
+import { insertCustomer } from '../actions/insertCustomer'
+
+import AppFrame from '../components/AppFrame'
+import CustomerEdit from '../components/CustomerEdit'
+
+class NewCustomerContainer extends Component {
+
+    /*
+    handleSubmit = values => {
+        // console.log(JSON.stringify(values))
+        const { id } = values
+        return this.props.updateCustomer(id, values)
+        .then(r => {
+          if(r.error && r.payload.error) { // r.error
+            throw new SubmissionError(r.payload.error) // r.payload
+          }
+        })
+    }
+  
+    handleOnSubmitSuccess = () => {
+        this.props.history.goBack()
+    }
+  
+    handleOnBack = () => {
+        this.props.history.goBack()
+    }
+    */
+
+    handleSubmit = values => {
+        return this.props.insertCustomer(values)
+        .then(r => {
+            if(r.payload && r.payload.error) { // r.error
+              throw new SubmissionError(r.payload) // r.payload
+            }
+        })
+    }
+  
+    handleOnSubmitSuccess = () => {
+        this.props.history.goBack()
+    }
+  
+    handleOnBack = () => {
+        this.props.history.goBack()
+    }
+
+    renderBody = () => {
+        return <CustomerEdit 
+        onSubmit={this.handleSubmit} 
+        onSubmitSuccess={this.handleOnSubmitSuccess}
+        onBack={this.handleOnBack}
+        />
+    }
+
+  render() {
+    return (
+      <div>
+          <AppFrame
+          header='Edicion del Cliente'
+          body={this.renderBody()}
+          >
+          </AppFrame>
+      </div>
+    )
+  }
+}
+
+NewCustomerContainer.propTypes = {
+    insertCustomer: PropTypes.func.isRequired,
+}
+
+export default withRouter(
+    connect(null, { insertCustomer })
+    (NewCustomerContainer)
+)
